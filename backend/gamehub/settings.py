8ebba80a9a398106291eb492a844546f2458e32c
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2025-07-01 15:39:31 +0330

    feat(backend): Caching settings & API_KEY set

--- backend/gamehub/settings.py
+++ backend/gamehub/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,5 +125,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173", # Your local Vite dev server
+    "http://localhost:5173",
 ]
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "TIMEOUT": 60 * 30,
+    }
+}
+
+RAWG_API_KEY = os.environ.get('RAWG_API_KEY')
