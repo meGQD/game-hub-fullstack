82db845c0337dc8adc203df5266f3e24cbdb0bee
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2025-07-02 20:27:51 +0330

    refactor(backend): Caching simplified

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,10 @@
 import requests
 from django.conf import settings
-from django.core.cache import cache
 from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
 
+@cache_page(60 * 15)
 def get_games(request):
-    cache_key = request.get_full_path() 
-
-    cached_data = cache.get(cache_key)
-    if cached_data:
-        return JsonResponse(cached_data, safe=False)
 
     api_url = 'https://api.rawg.io/api/games'
     params = request.GET.copy()
@@ -19,19 +15,13 @@ def get_games(request):
         response.raise_for_status()
         data = response.json()
 
-        cache.set(cache_key, data)
-
         return JsonResponse(data)
     
     except requests.exceptions.RequestException as e:
         return JsonResponse({'error': str(e)}, status=503)
-    
-def get_genres(request):
-    cache_key = request.get_full_path() 
 
-    cached_data = cache.get(cache_key)
-    if cached_data:
-        return JsonResponse(cached_data, safe=False)
+@cache_page(60 * 60 * 24)
+def get_genres(request):
 
     api_url = 'https://api.rawg.io/api/genres'
     params = request.GET.copy()
@@ -41,8 +31,6 @@ def get_genres(request):
         response = requests.get(api_url, params=params)
         response.raise_for_status()
         data = response.json()
-
-        cache.set(cache_key, data)
 
         return JsonResponse(data)
     
