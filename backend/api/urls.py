e00920d5672cf7e91fed66573bd0dd667f330337
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2025-07-02 14:34:47 +0330

    chore: Force remove __pycache__ from remote tracking

--- backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('games', views.get_games, name='get_games'),
-    path('genres', views.get_genres, name='get_genres'),
-]
