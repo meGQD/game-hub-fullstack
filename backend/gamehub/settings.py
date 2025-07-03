e413b7f33c6698f18b7b5ebedfeda30b3008db52
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2025-07-03 15:55:53 +0330

    feat(backend): Redis installed and configured for caching

--- backend/gamehub/settings.py
+++ backend/gamehub/settings.py
@@ -130,8 +130,11 @@ CORS_ALLOWED_ORIGINS = [
 
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-        "TIMEOUT": 60 * 30,
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
     }
 }
 
