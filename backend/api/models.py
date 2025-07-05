57b7c77ba4d31c1094383a81e87b4602ee6c0aa4
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2025-07-05 15:01:28 +0330

    feat(backend): Database models set

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    image_background = models.URLField(max_length=500, null=True, blank=True)
+
+class Platform(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+class Game(models.Model):
+    name = models.CharField(max_length=255)
+    metacritic = models.IntegerField()
+    rating_top = models.IntegerField()
+    background_image = models.URLField(max_length=500, null=True, blank=True)
+    genre = models.ManyToManyField(Genre, related_name='games')
+    parent_platforms = models.ManyToManyField(Platform, related_name='games')
