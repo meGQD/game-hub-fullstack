6c59bb6ac21a930ee4cdcd520d295ea92988a88c
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2025-07-07 17:41:16 +0330

    feat(backend): Caching set for new APIs

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,15 @@
 from django.db.models.aggregates import Count
 from django_filters.rest_framework import DjangoFilterBackend
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.filters import OrderingFilter, SearchFilter
 from .models import Game, Genre
 from .serializers import GameSerializer, GenreSerializer
-    
+
+@method_decorator(name='dispatch', decorator=vary_on_headers('Accept'))    
+@method_decorator(name='dispatch', decorator=cache_page(60 * 30))
 class GameViewSet(ReadOnlyModelViewSet):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
@@ -17,6 +22,8 @@ class GameViewSet(ReadOnlyModelViewSet):
 
     filterset_fields = ['genres', 'parent_platforms']
 
+@method_decorator(name='dispatch', decorator=vary_on_headers('Accept'))    
+@method_decorator(name='dispatch', decorator=cache_page(60 * 60 * 24))
 class GenreViewSet(ReadOnlyModelViewSet):
     queryset = Genre.objects.annotate(games_count=Count('games')).all()
     serializer_class = GenreSerializer
