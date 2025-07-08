d7339b4360a8de7bb1942ff57ba0ef5d303900f0
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2025-07-08 13:57:07 +0330

    refactor(backend): Nested Routing implemented

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework_nested import routers
 from . import views
 
-urlpatterns = [
-    path('games', views.GameViewSet.as_view({'get': 'list'}), name='get_games'),
-    path('genres', views.GenreViewSet.as_view({'get': 'list'}), name='get_genres'),
-]
+router = routers.DefaultRouter()
+router.register('games', views.GameViewSet, basename='games')
+router.register('genres', views.GenreViewSet, basename='genres')
+
+urlpatterns = router.urls
