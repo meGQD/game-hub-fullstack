d32da47dc5d624f818f042f6f7cdc6e9510173b1
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2025-07-10 16:15:10 +0330

    feat(backend): Add ProfileViewSet with me action for user profile retrieval and update + Require authentication

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,4 +1,7 @@
+from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -6,3 +9,16 @@ from .serializers import ProfileSerializer
 class ProfileViewSet(ListModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    permission_classes = [IsAdminUser]
+
+    @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated])
+    def me(self, request): 
+        (profile, created) = Profile.objects.get_or_create(user_id= request.user.id)
+        if request.method == 'GET':
+            serializer = ProfileSerializer(profile)
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = ProfileSerializer(profile, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
