34a3486b7fad37a96d1c6802c93364f54ab3d591
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2025-07-13 15:16:32 +0330

    feat(frontend): Implement routing with react-router-dom and add HomePage and GameDetailPage components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "@/components/ui/provider";
 import "./index.css";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
