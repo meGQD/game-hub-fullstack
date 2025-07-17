7b874b35525e86c72e0d5dc5f2d3012ed15dbac8
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2025-07-17 16:29:54 +0330

    feat(frontend): Implement BackgroundImage component and remove background logic from App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,15 @@
-import { Box, Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
+import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GenreList from "./components/GenreList";
 import { Outlet } from "react-router-dom";
-import useGameQueryStore from "./store";
-import { useColorMode } from "./components/ui/color-mode";
+import BackgroundImage from "./components/BackgroundImage";
 
 function App() {
   const showAside = useBreakpointValue({ base: false, xl: true });
-  const backgroundImageUrl = useGameQueryStore((s) => s.backgroundImageUrl);
-
-  const { colorMode } = useColorMode();
-
-  const gradient =
-    colorMode === "dark"
-      ? "linear-gradient(to bottom, rgba(18, 18, 18, 0) 0%, rgba(18, 18, 18, 1) 60%)"
-      : "linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 60%)";
 
   return (
     <>
-      {backgroundImageUrl && (
-        <Box
-          position="absolute"
-          top={0}
-          left={0}
-          right={0}
-          bottom={0}
-          bgImage={`${gradient}, url(${backgroundImageUrl})`}
-          bgSize="contain"
-          bgRepeat="no-repeat"
-          filter=" brightness(0.6)"
-          zIndex={-1}
-        />
-      )}
+      <BackgroundImage />
       <Grid
         templateAreas={{ base: `"nav" "main"`, xl: `"nav nav" "aside main"` }}
         templateColumns={{
---
+++ frontend/src/components/BackgroundImage.tsx
@@ -0,0 +1,34 @@
+import { Box } from "@chakra-ui/react";
+import { useColorMode } from "./ui/color-mode";
+import useGameQueryStore from "@/store";
+
+const BackgroundImage = () => {
+  const backgroundImageUrl = useGameQueryStore((s) => s.backgroundImageUrl);
+  const { colorMode } = useColorMode();
+
+  const gradient =
+    colorMode === "dark"
+      ? "linear-gradient(to bottom, rgba(18, 18, 18, 0) 0%, rgba(18, 18, 18, 1) 60%)"
+      : "linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 60%)";
+
+  return (
+    <div>
+      {backgroundImageUrl && (
+        <Box
+          position="absolute"
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          bgImage={`${gradient}, url(${backgroundImageUrl})`}
+          bgSize="contain"
+          bgRepeat="no-repeat"
+          filter=" brightness(0.6)"
+          zIndex={-1}
+        />
+      )}
+    </div>
+  );
+};
+
+export default BackgroundImage;
