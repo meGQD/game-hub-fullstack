f1026446d54c9d904162a9fa97046c93a55af5f2
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2025-07-23 17:57:04 +0330

    feat(frontend): Dynamically render profile icon based on user's name

--- frontend/src/features/users/components/ProfileIcon.tsx
+++ frontend/src/features/users/components/ProfileIcon.tsx
@@ -1,12 +1,17 @@
+import useAppStore from "@/store";
 import { Avatar } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const ProfileIcon = () => {
+  const user = useAppStore((s) => s.auth.user);
+
   return (
     <div>
       <Link to={"/profile"}>
-        <Avatar.Root>
-          <Avatar.Fallback name="mamad karimi" />
+        <Avatar.Root colorPalette={user ? "cyan" : ""}>
+          <Avatar.Fallback
+            name={user ? `${user.first_name} ${user.last_name}` : ""}
+          />
         </Avatar.Root>
       </Link>
     </div>
