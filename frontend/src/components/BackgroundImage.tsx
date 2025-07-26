853e8ce707a14ac155fcd8617bdc9c538decfff9
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2025-07-26 17:24:34 +0330

    feat(frontend): Dynamic gradienting for BackgroundImage configured

--- frontend/src/components/BackgroundImage.tsx
+++ frontend/src/components/BackgroundImage.tsx
@@ -6,11 +6,6 @@ const BackgroundImage = () => {
   const backgroundImageUrl = useAppStore((s) => s.backgroundImageUrl);
   const { colorMode } = useColorMode();
 
-  const gradient =
-    colorMode === "dark"
-      ? "linear-gradient(to bottom, rgba(18, 18, 18, 0) 0%, rgba(18, 18, 18, 1) 60%)"
-      : "linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 60%)";
-
   return (
     <div>
       {backgroundImageUrl && (
@@ -20,12 +15,31 @@ const BackgroundImage = () => {
           left={0}
           right={0}
           bottom={0}
-          bgImage={`${gradient}, url(${backgroundImageUrl})`}
+          bgImage={`url(${backgroundImageUrl})`}
           bgSize="contain"
           bgRepeat="no-repeat"
           filter={{ base: "brightness(1)", _dark: "brightness(0.4)" }}
           zIndex={-1}
-        />
+        >
+          <Box
+            // Handle dynamic gradient
+            position="absolute"
+            top={0}
+            left={0}
+            right={0}
+            bottom={0}
+            bgImage={{
+              base:
+                colorMode === "dark"
+                  ? "linear-gradient(to bottom, rgba(18, 18, 18, 0) 0%, rgba(18, 18, 18, 1) 20%)"
+                  : "linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 20%)",
+              lg:
+                colorMode === "dark"
+                  ? "linear-gradient(to bottom, rgba(18, 18, 18, 0) 0%, rgba(18, 18, 18, 1) 60%)"
+                  : "linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 60%)",
+            }}
+          />
+        </Box>
       )}
     </div>
   );
