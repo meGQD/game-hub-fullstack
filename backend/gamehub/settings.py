765ba07e3c643ba6c89767992a8de6fa6c1a4b83
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2025-07-27 16:47:29 +0330

    feat(backend): Postgresql data base configured to use

--- backend/gamehub/settings.py
+++ backend/gamehub/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,8 +87,12 @@ WSGI_APPLICATION = 'gamehub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST", default='localhost'),
+        'PORT': config("DB_PORT", default='5432'),
     }
 }
 
@@ -147,7 +152,7 @@ CACHES = {
     }
 }
 
-RAWG_API_KEY = os.environ.get('RAWG_API_KEY')
+RAWG_API_KEY = config('RAWG_API_KEY')
 
 REST_FRAMEWORK = {
     # 'DEFAULT_RENDERER_CLASSES': [
