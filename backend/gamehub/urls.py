87178c04feafa18f1255c6801c037fad710c7a97
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2025-07-30 16:16:06 +0330

    refactor(backend): Only include django-debug-toolbar when DEBUG is True

--- backend/gamehub/urls.py
+++ backend/gamehub/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-import debug_toolbar
 
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
@@ -24,5 +24,9 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('__debug__', include(debug_toolbar.urls))
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__', include('debug_toolbar.urls'))
+    ]
